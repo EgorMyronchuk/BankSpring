a6b9ab2038c5572ac429437f81b9f26c76c1ee18
egormyronchuk <egormyronchuk@gmail.com>
2024-07-12 14:56:18 +0300

    14:56 12:07

--- src/main/java/service/AccountService.java
+++ src/main/java/service/AccountService.java
@@ -2,14 +2,33 @@ package service;
 
 import dao.AccountDao;
 import lombok.RequiredArgsConstructor;
+import model.Account;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AccountService {
     private final AccountDao accountDao;
 
-    public void deposit(String accountNumber , Double amount) {
+    public Optional<Account> deposit(String accountNumber , Double amount) {
+        Optional<Account> account = accountDao.findAll()
+                .stream()
+                .filter(x -> x.getNumber().equals(accountNumber))
+                .findFirst();
+
+        if (account.isPresent()) {
+            Double balance = account.get().getBalance();
+            account.get().setBalance(balance + amount);
+            accountDao.save(account.get());
+        }
+        else ()
+
 
     }
+
+
+
+
 }
